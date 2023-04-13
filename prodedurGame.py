a1c35f16cd8a8b78e16328bffc01c4f29df8f939
SvanteAkesson <svante.akesson@sigma.se>
2023-04-13 19:08:54 +0200

    Kamel

--- prodedurGame.py
+++ prodedurGame.py
@@ -9,12 +9,12 @@ gameDisplay = pygame.display.set_mode((1000, 600))
 pygame.display.set_caption("Yo Game")
 
 
-jump_out = False
-while jump_out == False:
+jumpOut = False
+while jumpOut == False:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            jump_out = True
+            jumpOut = True
 
     gameDisplay.fill((100,100,200))
 
